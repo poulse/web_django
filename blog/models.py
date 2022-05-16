8b1d6305879dbd6e322b9a586e371dfc8cae39be
M1ddagh <misaelmiddagh29@gmail.com>
2022-05-16 10:25:45 -0400

    blog-1.2

---
+++ blog/models.py
@@ -0,0 +1,11 @@
+from importlib.resources import contents
+from statistics import mode
+from turtle import title
+from django.db import models
+
+class Post(models.Model):
+    title=models.CharField(max_length=250)
+    content=models.TextField()
+
+    def __str__(self):
+        return self.title
---
+++ blog/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from django.views.generic import View
+from .forms import PostCreateForm
+
+
+class BlogListView(View):
+    def get(self, request, *args, **kwargs):
+
+
+        context={
+
+        }
+
+        return render(request, 'blog_list.html', context)
+    
+class BlogCreateView(View):
+    def get(self, request, *args, **kwargs):
+        form= PostCreateForm
+        context={
+            'form':form
+        }
+        return render(request, 'blog_create.html', context)
+    
+    def post(self, request, *args, **kwargs):
+        context={
+
+        }
+        return render(request, 'blog_create.html', context)
