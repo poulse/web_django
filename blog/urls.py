ff3fa3817b7cccfb29b2dc0691bb769f24650d35
M1ddagh <misaelmiddagh29@gmail.com>
2022-07-06 20:20:10 -0400

    aunfalta0.1

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,6 @@
+from unicodedata import name
 from django.urls import path
-from .views import BlogListView, BlogCreateView
+from .views import BlogListView, BlogCreateView,BlogDetailView, BlogUpdateView, BlogDeleteView
 
 
 app_name='blog'
@@ -7,4 +8,7 @@ app_name='blog'
 urlpatterns= [
     path('',BlogListView.as_view(), name='home'),
     path('create/', BlogCreateView.as_view(), name='create'),
-]
+    path('<int:pk>/', BlogDetailView.as_view(), name='detail'),
+    path('<int:pk>/update/', BlogUpdateView.as_view(), name='update'),
+    path('<int:pk>/delete/', BlogDeleteView.as_view(), name='delete'),
+]   
--- blog/views.py
+++ blog/views.py
@@ -1,19 +1,20 @@
-from django.shortcuts import render
-from django.views.generic import View
+from turtle import title
+from django.shortcuts import get_object_or_404, redirect, render
+from django.urls import reverse_lazy
+from django.views.generic import View, UpdateView, DeleteView
 from .forms import PostCreateForm
-
+from .models import Post
 
 class BlogListView(View):
     def get(self, request, *args, **kwargs):
-
-
+        posts= Post.objects.all()
         context={
-
+            'posts':posts
         }
-
         return render(request, 'blog_list.html', context)
     
 class BlogCreateView(View):
+
     def get(self, request, *args, **kwargs):
         form= PostCreateForm
         context={
@@ -21,8 +22,38 @@ class BlogCreateView(View):
         }
         return render(request, 'blog_create.html', context)
     
-    def post(self, request, *args, **kwargs):
+    def post(self, request, *args, **kwargs): 
+        if request.method=='POST':
+            form= PostCreateForm(request.POST)
+            if form.is_valid():
+                title= form.cleaned_data.get('title')
+                content= form.cleaned_data.get('content')
+                p,created= Post.objects.get_or_create(title=title, content=content)
+                p.save()
+                return redirect('blog:home')
         context={
 
         }
         return render(request, 'blog_create.html', context)
+
+class BlogDetailView(View):
+    def get(self, request,pk, *args, **kwargs):
+        post=get_object_or_404(Post, pk=pk)
+        context={
+            'post':post
+        }
+        
+        return render(request, 'blog_detail.html', context)
+class BlogUpdateView(UpdateView):
+    model=Post
+    fields=['title','content']
+    template_name='blog_update.html'
+
+    def get_success_url(self):
+        pk=self.kwargs['pk']
+        return reverse_lazy('blog:detail',kwargs={'pk':pk})
+
+class BlogDeleteView(DeleteView):
+    model=Post
+    template_name= 'blog_delete.html'
+    success_url= reverse_lazy('blog:home')
